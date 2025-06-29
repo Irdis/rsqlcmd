df4a06f99818cebcf19e717e7e27e8b699df1db9
Ivan Novitskii <ivannovitskii@gmail.com>
2025-06-29 17:41:23 +0100

    add insert mode

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Globalization;
+using System.Text;
 using System.Text.RegularExpressions;
 using Microsoft.Data.SqlClient;
 
@@ -10,11 +12,15 @@ public class RSqlCmdArgs
     public string File { get; set; }
     public string Script { get; set; }
     public bool NoNewLines { get; set; }
+    public bool InsertMode { get; set; }
 }
 
 public class Program
 {
     public static Regex _go = new Regex(@"^\s*GO\s*;*\s*(--.*)?(\\\*.*)?$", RegexOptions.IgnoreCase);
+    private const string NoNameColumn = "NoName{0}";
+    private static readonly int NewLineLength = Environment.NewLine.Length;
+
     public static async Task Main(string[] args)
     {
         if (!ParseArgs(args, out var sqlCmdArgs))
@@ -54,6 +60,11 @@ public class Program
                 ind++;
                 res.NoNewLines = true;
             }
+            else if (arg == "-i")
+            {
+                ind++;
+                res.InsertMode = true;
+            }
             else if (arg == "-?")
             {
                 PrintUsage(Console.Out);
@@ -88,6 +99,7 @@ public class Program
         tw.WriteLine("    -f <file>:   file path");
         tw.WriteLine("    -s <script>: raw script");
         tw.WriteLine("    -nnl:        no new lines");
+        tw.WriteLine("    -i:          generate inserts");
         tw.WriteLine("    -?:          show this message");
     }
 
@@ -104,17 +116,160 @@ public class Program
             var command = connection.CreateCommand();
             command.CommandText = commandText;
             await using var reader = await command.ExecuteReaderAsync();
+            var tableIndex = 1;
             while (true)
             {
-                var rowIndex = 1;
-                PrintHeader(reader);
-                while (await reader.ReadAsync())
-                {
-                    PrintRow(rowIndex++, reader, args);
-                }
+                await PrintTable(reader, args, tableIndex);
                 if (!(await reader.NextResultAsync()))
                     break;
+                tableIndex++;
             }
+        }
+    }
+
+    private static Task PrintTable(SqlDataReader reader, RSqlCmdArgs args, int tableIndex)
+    {
+        if (args.InsertMode)
+        {
+            return PrintTableInserts(reader, args, tableIndex);
+        }
+        return PrintTableText(reader, args);
+    }
+
+    private static async Task PrintTableInserts(SqlDataReader reader, RSqlCmdArgs args, int tableIndex)
+    {
+        var prefix = GenerateInsertPrefix(reader, tableIndex);
+        if (prefix == null)
+        {
+            Console.WriteLine($"-- table{tableIndex} empty");
+            Console.WriteLine();
+            return;
+        }
+
+        PrintTableDefinition(reader, tableIndex);
+        await PrintTableContent(reader, tableIndex, prefix, args);
+    }
+
+    private static async Task PrintTableContent(SqlDataReader reader, int tableIndex, string prefix, RSqlCmdArgs args)
+    {
+        const int maxRowsCount = 100;
+        var rowIndex = 0;
+
+        var sb = new StringBuilder();
+        sb.AppendLine(prefix);
+        while (await reader.ReadAsync())
+        {
+            PrintInsertRow(rowIndex++, reader, args, sb);
+            if (rowIndex == maxRowsCount)
+            {
+                Console.WriteLine(sb.ToString());
+                sb.Clear();
+                sb.AppendLine(prefix);
+                rowIndex = 0;
+                continue;
+            }
+            sb.AppendLine(",");
+        }
+        if (rowIndex > 0)
+        {
+            var trimCount = NewLineLength + 1;
+            sb.Remove(sb.Length - trimCount, trimCount);
+            Console.WriteLine(sb.ToString());
+        }
+        Console.WriteLine();
+    }
+
+    private static void PrintTableDefinition(SqlDataReader reader, int tableIndex)
+    {
+        var sb = new StringBuilder();
+        sb.AppendFormat("CREATE TABLE #table{0} (", tableIndex);
+        sb.AppendLine();
+        var schemaTable = reader.GetSchemaTable();
+        var missingNameCounter = 1;
+
+        for (int i = 0; i < reader.FieldCount; i++)
+        {
+            var row = schemaTable.Rows[i];
+            var name = (string)row["ColumnName"];
+            var dataType = (string)row["DataTypeName"].ToString();
+            var columnSize = (int)row["ColumnSize"];
+            sb.Append("    [");
+            sb.Append(GetColumnName(name, ref missingNameCounter));
+            sb.Append("] ");
+            sb.Append(dataType);
+            if (dataType == "varchar" ||
+                dataType == "nvarchar")
+            {
+                sb.Append("(");
+                if (columnSize == int.MaxValue)
+                    sb.Append("max");
+                else
+                    sb.Append(columnSize);
+                sb.Append(")");
+            }
+            if (dataType == "decimal")
+            {
+                var numericPrecision = (Int16)row["NumericPrecision"];
+                var numericScale = (Int16)row["NumericScale"];
+                sb.AppendFormat("({0}, {1})",  numericPrecision, numericScale);
+            }
+            sb.AppendLine(",");
+        }
+        sb.AppendLine(")");
+        Console.WriteLine(sb.ToString());
+    }
+
+    private static string GenerateInsertPrefix(SqlDataReader reader, int tableIndex)
+    {
+        if (reader.FieldCount == 0)
+            return null;
+
+        var prefix = new StringBuilder();
+        prefix.AppendFormat("INSERT INTO #table{0} (", tableIndex);
+
+        var missingNameCounter = 1;
+        for (int i = 0; i < reader.FieldCount; i++)
+        {
+            var name = GetColumnName(reader.GetName(i), ref missingNameCounter);
+            prefix.AppendFormat("[{0}], ", name);
+        }
+        prefix.Remove(prefix.Length - 2, 2);
+        prefix.Append(") VALUES");
+        return prefix.ToString();
+    }
+
+    private static void PrintInsertRow(int rowIndex, SqlDataReader reader, RSqlCmdArgs args, StringBuilder sb)
+    {
+        sb.Append("(");
+        for (int i = 0; i < reader.FieldCount; i++)
+        {
+            var val = reader.GetValue(i);
+            if (val == DBNull.Value)
+            {
+                sb.AppendFormat("NULL");
+            }
+            else
+            {
+                var str = ProcessValue(args, val);
+                sb.AppendFormat("'{0}'", str);
+            }
+
+            sb.Append(",");
+        }
+        if (sb[sb.Length - 1] == ',')
+        {
+            sb.Remove(sb.Length - 1, 1);
+        }
+        sb.Append(")");
+    }
+
+    private static async Task PrintTableText(SqlDataReader reader, RSqlCmdArgs args)
+    {
+        var rowIndex = 1;
+        PrintTextHeader(reader);
+        while (await reader.ReadAsync())
+        {
+            PrintTextRow(rowIndex++, reader, args);
         }
     }
 
@@ -152,25 +307,22 @@ public class Program
         }
     }
 
-    private static void PrintHeader(SqlDataReader reader)
+    private static void PrintTextHeader(SqlDataReader reader)
     {
         if (reader.FieldCount == 0)
             return;
         Console.Write("Table cols: ");
+        var missingNameCounter = 1;
         for (int i = 0; i < reader.FieldCount; i++)
         {
-            var name = reader.GetName(i);
-            if (string.IsNullOrEmpty(name))
-            {
-                name = "<no name>";
-            }
+            var name = GetColumnName(reader.GetName(i), ref missingNameCounter);
             Console.Write($"{i + 1}) {name} ");
         }
         Console.WriteLine();
         Console.WriteLine();
     }
 
-    private static void PrintRow(int rowIndex, SqlDataReader reader, RSqlCmdArgs args)
+    private static void PrintTextRow(int rowIndex, SqlDataReader reader, RSqlCmdArgs args)
     {
         Console.WriteLine($"Row index #{rowIndex}");
         for (int i = 0; i < reader.FieldCount; i++)
@@ -186,7 +338,7 @@ public class Program
     {
         if (value == DBNull.Value)
             return "<NULL>";
-        var str = value.ToString();
+        var str = GetObjectStrValue(value);
         if (args.NoNewLines)
         {
             var newLineIndex = str.IndexOf("\r\n");
@@ -201,5 +353,26 @@ public class Program
             str = str.Substring(0, zeroIndex);
         }
         return str;
+    }
+
+    private static string GetObjectStrValue(object value)
+    {
+        if (value is int intVal)
+            return intVal.ToString(CultureInfo.InvariantCulture);
+
+        if (value is decimal decimalVal)
+            return decimalVal.ToString(CultureInfo.InvariantCulture);
+
+        return value.ToString();
+    }
+
+    private static string GetColumnName(string columnName, ref int missingNameCounter)
+    {
+        return string.IsNullOrEmpty(columnName) ? GetNoNameColumn(missingNameCounter++) : columnName;
+    }
+
+    private static string GetNoNameColumn(int missingNameCounter)
+    {
+        return string.Format(NoNameColumn, missingNameCounter);
     }
 }
--- example.sql
+++ example.sql
@@ -1,1 +1,18 @@
-select 1 as Id, 'Ivan' as Name
+select 1 as Id, cast('Ivan' as varchar(40)) as Name, cast('Rosa' as varchar(max)) as Cat, cast(49.001 as decimal(21, 7)) as Size
+select 1, 'hello' as Title
+union all
+select 2, 'abc'
+union all
+select 3, 'abc'
+union all
+select 4, 'abc'
+union all
+select 5, 'abc'
+union all
+select 6, 'abc'
+union all
+select 7, 'abc'
+union all
+select 8, 'abc'
+union all
+select 9, 'abc'
